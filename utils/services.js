5ad3ee9669c71037200d323aed6103c0d892e48b
nicolasleal570 <lealnicolas96@gmail.com>
2020-06-01 02:22:34 -0400

    modifying expiration day

--- utils/services.js
+++ utils/services.js
@@ -5,7 +5,7 @@ module.exports = function (user) {
     const payload = {
         sub: user._id,
         iat: moment().unix(),
-        exp: moment().add(1, "minutes").unix(),
+        exp: moment().add(2, "day").unix(),
     };
     return jwt.encode(payload, process.env.TOKEN_SECRET);
 }
