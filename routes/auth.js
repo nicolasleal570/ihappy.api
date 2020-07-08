2156e640b8351e48ffe3679d29a59c6a4eafcb05
nicolasleal570 <lealnicolas96@gmail.com>
2020-07-07 22:58:36 -0400

    Removing cookies config

--- routes/auth.js
+++ routes/auth.js
@@ -3,9 +3,6 @@ const bcrypt = require("bcryptjs");
 const router = express.Router();
 const User = require("../models/User");
 const createToken = require("../utils/services");
-const { initSession } = require("../utils/utils");
-
-const isLoggedIn = require('../utils/verifyProtectedRoutes');
 
 // @desc    Register new user
 // @route   POST /api/auth/register/
@@ -26,22 +23,11 @@ router.post("/register", async (req, res) => {
     const salt = await bcrypt.genSalt(10);
     const hashPassword = await bcrypt.hash(password, salt);
 
-    let userExist = await User.findOne({ email });
-
-    if (userExist) {
-      userExist = null;
+    const emailExist = await User.findOne({ email: email });
+    if (emailExist) {
       return res
         .status(400)
-        .json({ success: false, error: "Email already exists" });
-    }
-
-    userExist = await User.findOne({ username });
-
-    if (userExist) {
-      userExist = null;
-      return res
-        .status(400)
-        .json({ success: false, error: "Username already exists" });
+        .json({ success: false, data: "Email already exists" });
     }
 
     let user = await User.create({
@@ -53,25 +39,17 @@ router.post("/register", async (req, res) => {
 
     user = await User.findById(user._id).populate("role");
 
-    const session = await initSession(user._id);
+    console.log(user);
 
     // Create JWT a token
-    // const token = createToken(user);
+    const token = createToken(user);
 
     // Send a new token to the client (frontend)
-    return res
-      .cookie("token", session.token, {
-        httpOnly: true,
-        sameSite: true,
-        maxAge: 1209600000,
-        secure: false,
-      })
-      .status(200)
-      .json({
-        success: true,
-        token: session.token,
-        user,
-      });
+    return res.status(200).json({
+      success: true,
+      token,
+      user,
+    });
   } catch (err) {
     if (err.name === "ValidationError") {
       const messages = Object.values(err.errors).map((val) => val.message);
@@ -113,24 +91,15 @@ router.post("/login", async (req, res) => {
     }
 
     // Create JWT a token
-    const session = await initSession(user._id);
     // const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
-    // const token = createToken(user);
+    const token = createToken(user);
 
     // Send a new token to the client (frontend)
-    return res
-      .cookie("token", session.token, {
-        httpOnly: true,
-        sameSite: true,
-        maxAge: 1209600000,
-        secure: false,
-      })
-      .status(200)
-      .json({
-        success: true,
-        token: session.token,
-        user,
-      });
+    return res.status(200).json({
+      success: true,
+      token,
+      user,
+    });
   } catch (err) {
     if (err.name === "ValidationError") {
       const messages = Object.values(err.errors).map((val) => val.message);
@@ -146,48 +115,6 @@ router.post("/login", async (req, res) => {
       });
     }
   }
-});
-
-// @desc    Get the authenticated user
-// @route   GET /api/auth/me/
-// @access  Private
-router.get("/me", isLoggedIn, async (req, res) => {
-  try {
-    const requestedUserID = req.user;
-    const user = await User.findById(requestedUserID).populate("role");
-
-    res.status(200).json({
-        success: true,
-        data: user
-      });
-  } catch (err) {
-    return res.status(401).json({
-      success: false,
-      error: "Not authorized to access this route",
-    });
-  }
-});
-
-// @desc    Logout user
-// @route   PUT /api/auth/logout/
-// @access  Private
-router.put("/logout", isLoggedIn, async (req, res) => {
-    try {
-        const { session } = req;
-        await session.expireToken(session.token);
-        res.clearCookie('token');
-
-        return res.status(200).json({
-            success: true,
-            data: 'Successfuly expired login session'
-        });
-
-    } catch (err) {
-        return res.status(500).json({
-            success: false,
-            error: 'Something went wrong during the logout process.'
-        })
-    }
 });
 
 module.exports = router;
--- utils/verifyProtectedRoutes.js
+++ utils/verifyProtectedRoutes.js
@@ -1,29 +1,20 @@
 const jwt = require("jwt-simple");
 const cookieParser = require("cookie-parser");
-const Session = require("../models/Session");
 
-module.exports = async function (req, res, next) {
+module.exports = function (req, res, next) {
   try {
-    const { token: tokenCookie } = req.cookies;
-    const session = await Session.findOne({
-      token: tokenCookie,
-      status: "valid",
-    });
-
-    if (!session) {
-      res.clearCookie("token");
-      return res.status(200).send({
+    if (!req.headers.authorization) {
+      return res.status(403).send({
         success: false,
-        error: "Your session has expired.",
+        error: "You have to be logged in",
       });
     }
-    req.session = session;
-    req.user = session.userId
-    // const token = req.headers.authorization.split(" ")[1];
 
-    // const payload = jwt.decode(token, process.env.TOKEN_SECRET);
+    const token = req.headers.authorization.split(" ")[1];
 
-    // req.user = payload.sub;
+    const payload = jwt.decode(token, process.env.TOKEN_SECRET);
+
+    req.user = payload.sub;
     next();
   } catch (err) {
     return res.status(401).send({
