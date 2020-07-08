f598679f27414ea46c5c606a01969b3b333a365d
Gianluca Di Bella <gianluca.dib99@gmail.com>
2020-07-07 23:38:18 -0400

    factura funcional

--- routes/factura.js
+++ routes/factura.js
@@ -61,57 +61,57 @@ router.post('/', isLoggedIn, async (req, res) => {
   }
 })
 
-// router.post('/', async (req, res) => {
+router.post('/', async (req, res) => {
 
-//   try {
-//     // Destructuring de lo que manda el usuario
-//     const {
-//         slug_psicologo,
-//         id_usuario,
-//         fecha,
-//         total,
-//         id_tarjeta
-//     } = req.body
+  try {
+    // Destructuring de lo que manda el usuario
+    const {
+        slug_psicologo,
+        id_usuario,
+        fecha,
+        total,
+        id_tarjeta
+    } = req.body
 
-//     const psicologo = await User.findOne({ slug: slug_psicologo });
+    const psicologo = await User.findOne({ slug: slug_psicologo });
 
-//     const factura = await Factura.create({
-//         id_psicologo: psicologo._id,
-//         id_usuario,
-//         fecha,
-//         total,
-//         id_tarjeta
-//     });
+    const factura = await Factura.create({
+        id_psicologo: psicologo._id,
+        id_usuario,
+        fecha,
+        total,
+        id_tarjeta
+    });
 
-//     const Allfactura = await Factura.findOne(factura).populate('user');
+    const Allfactura = await Factura.findOne(factura).populate('user');
 
-//     return res.status(200).json({
-//       success: true,
-//       data: Allfactura
-//     });
+    return res.status(200).json({
+      success: true,
+      data: Allfactura
+    });
 
-//   } catch (err) {
-//     if (err.name === 'ValidationError') {
-//       const messages = Object.values(err.errors).map(val => val.message);
+  } catch (err) {
+    if (err.name === 'ValidationError') {
+      const messages = Object.values(err.errors).map(val => val.message);
 
-//       return res.status(400).json({
-//         success: false,
-//         error: messages
-//       });
-//     } else {
-//       return res.status(500).json({
-//         success: false,
-//         error: 'Server Error'
-//       });
-//     }
-//   }
+      return res.status(400).json({
+        success: false,
+        error: messages
+      });
+    } else {
+      return res.status(500).json({
+        success: false,
+        error: 'Server Error'
+      });
+    }
+  }
 
-// });
+});
 
 // @desc    Filter reviews by psychologist
 // @route   POST /api/reviews/psychologist/
 // @access  Private
-router.get('/find/:psychologist', isLoggedIn, async (req, res) => {
+router.get('/:psychologist', isLoggedIn, async (req, res) => {
 
   try {
 
@@ -131,7 +131,8 @@ router.get('/find/:psychologist', isLoggedIn, async (req, res) => {
     res.status(200).json({
       success: true,
       data: {
-        factura
+        factura,
+        psicologo
       }
     });
 
